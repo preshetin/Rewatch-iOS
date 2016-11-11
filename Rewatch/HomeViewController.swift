a0a6e8756bd40d2a5fbb5205ea25917675a495cb
Petr Reshetin <preshetin@gmail.com>
2016-11-11 14:12:20 +0300

    Basic view controllers

--- Rewatch/HomeViewController.swift
+++ Rewatch/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  SecondViewController.swift
+//  FirstViewController.swift
 //  Rewatch
 //
 //  Created by Petr Reshetin on 11/11/2016.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
